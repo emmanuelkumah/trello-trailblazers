f62bc20b59de95e6fd2276844427c8c17f2f87e8
Fasakin Henry <fasakinhenry@gmail.com>
2025-03-29 09:19:47 +0100

    fix: fix the shadcn button error

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,7 +1,18 @@
-export default function LandingPage() {
+import LayoutWrapper from "@/layout/wrapper/LayoutWrapper";
+import HeroSection from "./sections/HeroSection";
+import FeaturesSection from "./sections/FeaturesSection";
+// import TestimonialsSection from "./sections/TestimonialsSection";
+// import CTASection from "./sections/CTASection";
+
+const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing page</h1>
-    </div>
+    <LayoutWrapper>
+      <HeroSection />
+      <FeaturesSection />
+      {/* <TestimonialsSection />
+      <CTASection /> */}
+    </LayoutWrapper>
   );
-}
+};
+
+export default LandingPage;
