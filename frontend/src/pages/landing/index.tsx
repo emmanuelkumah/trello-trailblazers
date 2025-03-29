9b22f59fb48f757ef8e9d83fd2396d078bedb0b8
Fasakin Henry <fasakinhenry@gmail.com>
2025-03-29 09:31:26 +0100

    feat: update the landing page with all sections

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,16 +1,16 @@
 import LayoutWrapper from "@/layout/wrapper/LayoutWrapper";
 import HeroSection from "./sections/HeroSection";
 import FeaturesSection from "./sections/FeaturesSection";
-// import TestimonialsSection from "./sections/TestimonialsSection";
-// import CTASection from "./sections/CTASection";
+import TestimonialsSection from "./sections/TestimonialsSection";
+import CTASection from "./sections/CTASection";
 
 const LandingPage = () => {
   return (
     <LayoutWrapper>
       <HeroSection />
       <FeaturesSection />
-      {/* <TestimonialsSection />
-      <CTASection /> */}
+      <TestimonialsSection />
+      <CTASection />
     </LayoutWrapper>
   );
 };
