578fe2f68df9aff42126209a33f71fd90a9b7a5f
Fasakin Henry <fasakinhenry@gmail.com>
2025-04-07 01:34:22 +0100

    chore: add fonts to the fonts folder

--- frontend/src/ui/landing/HeroSection.tsx
+++ frontend/src/ui/landing/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className='container mx-auto px-4'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
           <div>
-            <h1 className='text-4xl md:text-5xl font-bold mb-4'>
+            <h1 className='text-4xl md:text-5xl mb-4 font-normal'>
               Effortless Expense Sharing for Every Occasion
             </h1>
           </div>
