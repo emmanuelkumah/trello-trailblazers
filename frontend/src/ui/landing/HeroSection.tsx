9ee49028f963b7dfa1c2ea1f3fdeddfd1a5981de
Fasakin Henry <fasakinhenry@gmail.com>
2025-04-07 02:25:19 +0100

    style: update font for hero

--- frontend/src/ui/landing/HeroSection.tsx
+++ frontend/src/ui/landing/HeroSection.tsx
@@ -12,20 +12,20 @@ const HeroSection = () => {
             </h1>
           </div>
           <div>
-            <p className='text-lg mb-6'>
+            <p className='text-lg mb-6 font-sarabun'>
               Divide bills and expenses simply, keep track of IOUs, and settle
               costs between friends, family, and roommates. Simplify expense
               splitting for trips, dinners, and any shared expenses.
             </p>
             <div className='flex items-center'>
-              <Button className='bg-[#FF8E8E] hover:bg-[#FF7070] text-white rounded-full px-6'>
+              <Button className='bg-[#FF8E8E] hover:bg-[#FF7070] text-white rounded-full px-6 font-sarabun'>
                 Get Started
               </Button>
               <Button
                 variant='outline'
-                className='ml-4 text-gray-700 rounded-full border-2 border-gray-300 px-6 bg-transparent hover:bg-transparent'
+                className='ml-4 text-gray-700 rounded-full border-2 border-gray-300 px-6 bg-transparent hover:bg-transparent font-sarabun'
               >
-                Learn More →
+                Learn More
               </Button>
             </div>
           </div>
