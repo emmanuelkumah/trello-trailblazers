295adcd86ce277bcf729230a01de45a631d1042d
Fasakin Henry <fasakinhenry@gmail.com>
2025-04-27 10:55:52 +0100

    fix: update cors origin

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -14,24 +14,25 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: ['http://localhost:5173', 'https://trellodivvy.vercel.app'], // Add production URL here
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/otp", require("./routes/OTPRoutes"));
-app.use("/api/groups", require("./routes/groupRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/expenses", require("./routes/expenseRoutes"));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/otp', require('./routes/OTPRoutes'));
+app.use('/api/groups', require('./routes/groupRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/expenses', require('./routes/expenseRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
